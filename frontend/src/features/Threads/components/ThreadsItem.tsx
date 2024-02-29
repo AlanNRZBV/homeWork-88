e0005d8fd6894366fa65a116fad22bcc0ef75881
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-03-01 03:08:07 +0600

    frontend.feat: added AddThreadForm component

---
+++ frontend/src/features/Threads/components/ThreadsItem.tsx
@@ -0,0 +1,67 @@
+import { FC } from 'react';
+import moment from 'moment';
+import { apiURL } from '../../../constants.ts';
+import { Thread } from '../../../types';
+import { Box, CircularProgress, Typography } from '@mui/material';
+import ForumIcon from '@mui/icons-material/Forum';
+import { Image } from 'mui-image';
+import { NavLink } from 'react-router-dom';
+
+const ThreadsItem: FC<Thread> = ({
+  _id,
+  image,
+  title,
+  description,
+  datetime,
+}) => {
+  const date = moment(datetime).format('MMMM Do YYYY, h:mm:ss a');
+
+  return (
+    <Box
+      gridColumn="span 4"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      p={1}
+      sx={{ border: '1px solid grey', borderRadius: '12px' }}
+    >
+      {image ? (
+        <Image
+          src={`${apiURL}/${image}`}
+          alt="Thread image"
+          fit="cover"
+          width="25%"
+          showLoading={<CircularProgress />}
+          style={{ borderRadius: '4px' }}
+        />
+      ) : (
+        <ForumIcon fontSize="large" sx={{ flexGrow: 1 }} />
+      )}
+      <Box
+        display="flex"
+        alignSelf="stretch"
+        flexDirection="column"
+        flexBasis="75%"
+        ml={2}
+        overflow="hidden"
+      >
+        <Typography variant="h6">{title}</Typography>
+        <Typography color="gray" variant="caption">
+          {date}
+        </Typography>
+        <Typography
+          component={NavLink}
+          to={`/threads/${_id}`}
+          variant="body2"
+          mt={2}
+          mb={2}
+          noWrap={true}
+        >
+          {description}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default ThreadsItem;
